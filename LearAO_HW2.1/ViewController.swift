a62dfec8a8ddaea28657e420028528d00a0dde92
Andrey Lear <learandrey@gmail.com>
2020-08-20 23:38:38 +1000

    Complete

--- LearAO_HW2.1/ViewController.swift
+++ LearAO_HW2.1/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        yellowView.layer.cornerRadius = 16
+        blueView.layer.cornerRadius = 16
+        
+        
     }
 
 
